55115d2c692b31658ef94c258c2a072bbe9afe20
Weverton Santos <wevertontevez0@gmail.com>
2026-05-23 03:52:35 -0300

    Delete backend directory

--- backend/src/main/java/br/edu/fatecpg/vivapg/repository/BairroRepository.java
+++
@@ -1,8 +0,0 @@
-package br.edu.fatecpg.vivapg.repository;
-import br.edu.fatecpg.vivapg.model.Bairro;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-public interface BairroRepository extends MongoRepository<Bairro, String> {
-    List<Bairro> findByRegiao(String regiao);
-    List<Bairro> findByTemPraiaTrue();
-}
